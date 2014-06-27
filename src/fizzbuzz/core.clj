77ca0a94359517b43f3eaa3f635a383ea3e515ed
zeroDivisible <zerodivisible@gmail.com>
2014-06-27 23:14:25 +0100

    implemented my first passing test for clojure

--- src/fizzbuzz/core.clj
+++ src/fizzbuzz/core.clj
@@ -1,10 +1,12 @@
 (ns fizzbuzz.core
   (:gen-class))
 
+(defn generate-numbers []
+  (range 1 101))
+
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
   (println "Hello, World!"))
 
-(defn generate-numbers
-  (range 1 101))
+
--- test/fizzbuzz/core_test.clj
+++ test/fizzbuzz/core_test.clj
@@ -2,10 +2,9 @@
   (:require [clojure.test :refer :all]
             [fizzbuzz.core :refer :all]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
 
-(deftest it-should-print-100-numbers
-  (testing "should print numbers from 1 to 100"
-    (is (= generate-numbers (range 1 101)))))
+(deftest generate-numbers-function
+  (testing "should return numbers from 1 to 100"
+    (is (map = generate-numbers (range 1 101)))))
+
+
