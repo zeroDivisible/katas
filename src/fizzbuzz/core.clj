13e9493f28928e0ee665737ccdca016fda6e05b0
zeroDivisible <zerodivisible@gmail.com>
2014-06-28 00:37:57 +0100

    code style

--- src/fizzbuzz/core.clj
+++ src/fizzbuzz/core.clj
@@ -1,13 +1,13 @@
 (ns fizzbuzz.core
   (:gen-class))
 
-(defn by [numb arg]
+(defn by? [numb arg]
   (= 0 (mod numb arg)))
 
 (defn fizzbuzz [col]
-  (map #(if (by % 15) "FizzBuzz"
-            (if (by % 5) "Buzz"
-            (if (by % 3) "Fizz" %)))
+  (map #(if (by? % 15) "FizzBuzz"
+            (if (by? % 5) "Buzz"
+            (if (by? % 3) "Fizz" %)))
        col))
 
 
