aecd1c72d29fb41d683cd7f37f561baddc115046
zeroDivisible <zerodivisible@gmail.com>
2014-07-01 11:32:14 +0100

    WIP working on addiion on an interface

--- clojure/singly-linked-lists/src/lists/core.clj
+++ clojure/singly-linked-lists/src/lists/core.clj
@@ -1,4 +1,15 @@
 (ns lists.core
   (:gen-class))
 
-(deftype Node [car cdr])
+(definterface INode
+  (getCar [])
+  (setCar [x])
+  (getCdr [])
+  (setCdr [x]))
+
+
+(deftype Node [^:volatile-mutable car ^:volatile-mutable cdr] INode
+  (getCar [this] car)
+  (setCar [this x] (set! car x) this)
+  (getCdr [this] cdr)
+  (setCdr [this x] (set! cdr x) this))
--- clojure/singly-linked-lists/test/lists/core_test.clj
+++ clojure/singly-linked-lists/test/lists/core_test.clj
@@ -1,16 +1,17 @@
 (ns lists.core-test
   (:require [clojure.test :refer :all]
             [lists.core :refer :all])
-  (:import lists.core.Node))
+  (:import [lists.core Node]))
 
 (deftest node-tests
   (testing "should be able to create a Node object"
     (is (not= (type Node) nil)))
   
   (testing "should properly pass 1 to car for a node"
-    (is (= 1 (let [node (Node. 1 2)] (.car node)))))
+    (is (= 1 (let [node (Node. 1 2)] (.getCar node)))))
 
   (testing "should properly pass 2 to cdr for a node"
-    (is (= 2 (let [node (Node. 1 2)] (.cdr node)))))
+    (is (= 2 (let [node (Node. 1 2)] (.getCdr node)))))
 
-  )
+  (testing "car should be volatile"
+    (is (= true ((meta (Node. 1 2)) :volatile-mutable)))))
